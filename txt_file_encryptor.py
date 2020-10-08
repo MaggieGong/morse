47e51f81b6039bb01ce027eb1799a8af265e1923
Manganese <maggiegongsh@gmail.com>
2020-10-07 21:29:12 -0700

    txt_file_encryptor

---
+++ txt_file_encryptor.py
@@ -0,0 +1,28 @@
+import os
+
+morse = {
+    "A":".-", "B":"-...", "C":"-.-.", "D":"-..", "E":".", "F":"..-.", "G":"--.", "H":"....", "I":"..", "J":".---", "K":"-.-", "L":".-..", "M":"--", "N":"-.", "O":"---", "P":".--.", "Q":"--.-", "R":".-.", "S":"...", "T":"-", "U":"..-", "V":"...-", "W":".--", "X":"-..-", "Y":"-.--", "Z":"--..",
+}
+
+print("# make sure you have put the file you want to encrypt in the same directory #")
+file_name = str(input('file name: '))
+
+here = os.path.dirname(os.path.abspath(__file__))
+file = os.path.join(here, file_name)
+text = open(file if ".txt" in file else file + ".txt", "r").read()
+
+text = text.upper()
+encrypted_text, index = "", 0
+count = 0
+
+for _ in text:
+    if _ in morse:
+        encrypted_text += (morse[_] + " ")
+    elif _ == " ":
+        encrypted_text += "  "
+    elif _ == ".":
+        encrypted_text += "'"
+    else:
+        encrypted_text += (_ + " ")
+print("\n" + "ORIGINAL:" + "\n" + text)
+print("\n" + "ENCRYPTED:" + "\n" + encrypted_text)
